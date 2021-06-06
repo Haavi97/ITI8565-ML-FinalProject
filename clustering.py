ad0c18dca73653dccc787e741ed8b7e9bdfa6221
fortin <fortin@ttu.ee>
2021-06-06 23:28:44 +0300

    silhouette_coefficients added in clustering

--- clustering.py
+++ clustering.py
@@ -2,6 +2,7 @@ from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 from sklearn.metrics import classification_report
 from sklearn.model_selection import cross_val_score
+from kneed import KneeLocator
 import matplotlib.pyplot as plt
 
 from plotting import maximize_screen
@@ -15,10 +16,14 @@ def clustering(scaled_features):
         "random_state": 42,
     }
     sse = []
+    silhouette_coefficients = []
     for k in range(1, 11):
         kmeans = KMeans(n_clusters=k, **kmeans_kwargs)
         kmeans.fit(scaled_features)
         sse.append(kmeans.inertia_)
+        if k>1:
+            score = silhouette_score(scaled_features, kmeans.labels_)
+            silhouette_coefficients.append(score)
     plt.style.use("fivethirtyeight")
     plt.plot(range(1, 11), sse)
     plt.xticks(range(1, 11))
@@ -26,9 +31,14 @@ def clustering(scaled_features):
     plt.ylabel("SSE")
     maximize_screen()
     plt.show()
+    plt.style.use("fivethirtyeight")
+    plt.plot(range(2, 11), silhouette_coefficients)
+    plt.xticks(range(2, 11))
+    plt.xlabel("Number of Clusters")
+    plt.ylabel("Silhouette")
+    maximize_screen()
+    plt.show()
     kl = KneeLocator(
         range(1, 11), sse, curve="convex", direction="decreasing"
     )
-    print('Best number of clusters: {}'.format(k1.elbow))
-    print('Kmeans cross validation report:')
-    print(cross_val_score(KMeans(k), scaled_features))
+    print('Best number of clusters: {}'.format(kl.elbow))
